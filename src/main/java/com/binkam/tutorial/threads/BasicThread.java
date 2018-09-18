96c41e3f9a9e613ee7581a032759b06a405f03c8
binkam <ceo@binkam.com>
2018-09-18 22:09:05 +0530

    Added Basic Thread class

--- src/main/java/com/binkam/tutorial/threads/BasicThread.java
+++ src/main/java/com/binkam/tutorial/threads/BasicThread.java
@@ -1,7 +1,15 @@
 package com.binkam.tutorial.threads;
 
+/**
+ * This class demonstrates a simple thread concept.
+ */
 public class BasicThread {
 
+    /**
+     * This class is an inner class which implements Runnable.
+     * This method prints the string that is passed to the
+     * constructor.
+     */
     private static class PrintJob implements Runnable{
         private String printable;
         public PrintJob(String printable){
@@ -9,13 +17,25 @@ public class BasicThread {
         }
         @Override
         public void run() {
-            //System.out.println("------Printing the printable-----");
             System.out.println(">>>>------Thread: "+Thread.currentThread().getId()+"----->"+this.printable);
-            //System.out.println("-----------");
-            //System.out.println("----------------------------------");
         }
     }
 
+    /**
+     * This the main method.
+     * We are defining 4 thread objects with PrintJob.
+     * Even though we run the threads 1,2,3,4 order,
+     * when it is run in real time, the order is not guaranteed.
+     *
+     *
+     * Ex. Output would be as below.
+     *  >>>>------Thread: 11-----> Binkam 1
+     *  >>>>------Thread: 14-----> Binkam 4
+     *  >>>>------Thread: 13-----> Binkam 3
+     *  >>>>------Thread: 12-----> Binkam 2
+     *  
+     * @param args
+     */
     public static void main(String[] args) {
         Thread t = new Thread(new PrintJob(" Binkam 1"));
         Thread t1 = new Thread(new PrintJob(" Binkam 2"));
